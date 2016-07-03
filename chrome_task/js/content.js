7f2c00a693f48be9bd5710dc68ec307d6b57c314
pangyongkun <119696189178@qq.com>
2016-07-03 10:38:39 +0800

    过滤和翻译效果实现
    翻译里面的音频没有搞定，有一些小问题，分页阅读没有实现

--- chrome_task/js/content.js
+++ chrome_task/js/content.js
@@ -2,54 +2,80 @@
  * Created by kun on 2016/7/2.
  */
 
-$(function(){
+$(function () {
 
-    /*隐藏不需要的内容*/
-    $(".top-banner-ad-container").hide();
-
-    $(".js-navigation-header").hide();
-    $(".content__labels").hide();
-    $(".content__meta-container").hide();
-    $(".content__secondary-column").hide();
-
-    $(".content-footer ").hide();
-    $(".l-footer__primary ").hide();
-
-    $("footer").hide();
-
-    $(".site-message").remove();
-
-    $(".submeta").hide();
-
-    $("aside").removeAttr("class").css("display","none");
+    /*移除不需要的内容*/
+    $(".top-banner-ad-container," +
+        ".js-navigation-header," +
+        ".content__labels," +
+        ".content__meta-container," +
+        ".content__secondary-column," +
+        ".content-footer," +
+        ".l-footer__primary," +
+        "footer,.site-message," +
+        ".submeta," +
+        "aside")
+        .remove();
 
 
+    /*翻译*/
+    $("body").mouseup(function (e) {
+        var x = 10;
+        var y = 10;
 
-    //添加选中事件
-    document.onmouseup = surroundContent;
+        //页面可见高宽
+        var docX = document.documentElement.clientWidth;
+        var docY = document.documentElement.clientHeight;
+
+        var text = "";
+        if (document.selection) {
+            text = document.selection.createRange().text;
+        }
+        else if (window.getSelection()) {
+            text = window.getSelection();
+        }
+        if (text != "") {
+
+            $("body").on("click","#tooltip",function(){
+                alert("hello");
+            })
+
+            $.get("https://api.shanbay.com/bdc/search/?word=" + text, function (data) {
+                var audioUrl = data.data.audio;
+
+                var tooltip = "<div id='tooltip' class='tooltip tooltip_container' ><img id='play' src='http://pic.qiantucdn.com/58pic/14/40/27/22Y58PICrm2_1024.png' style='width:12px;height:12px'/>"
+                    + data.data.definition +
+                    "</div>";
+                $("body").append(tooltip);
 
 
-    //获取选中内容
-    function surroundContent() {
-        var r;
-        if (window.getSelection) {
-            r = window.getSelection();
-            if (r.rangeCount > 0) {
-                r = r.getRangeAt(0);
+                $("#tooltip")
+                    .css({
+                        "top": (e.pageY + y) + "px",
+                        "left": (e.pageX + x) + "px",
+                    })
+                    .show("fast")
+                    .on("click", function () {
+                        alert("hello")
+                    });
 
-                translate(r);
-
-            }
+            });
 
         }
-    }
+    }).mousedown(function () {
+        $("#tooltip").remove();
+    });
 
-    function translate(word){
 
-        $.get("https://api.shanbay.com/bdc/search/?word="+word,function(data){
+    function doit(url) {
 
-            alert(data.data.definition);
-        });
+        console.log("hello");
+
+        alert(url);
+        /*var audio;
+         audio = document.createElement("audio");
+         audio.src = url;
+         audio.play();*/
 
     }
 
@@ -57,5 +83,4 @@ $(function(){
 })
 
 
-//shanbei请求
 
